7d3d370f0b011a4de64fc6baaa4c026efc6a337e
Daniel Aparisi <daniel.aparisi.lozano@gmail.com>
2026-08-18 09:05:30 +0200

    adding recongnize

---
+++ main.js
@@ -0,0 +1,10 @@
+function cretaeXmastree(height){
+
+    const lineas = height + 2
+    for(let i = 0; i < lineas; i++){
+        for(let j = i; j<lineas;j ++){
+            return "*"
+        }
+    }
+}
+cretaeXmastree(3)
